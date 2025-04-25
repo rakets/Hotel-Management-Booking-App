f376b6830d757e436a855e64f7221095cc0237b6
Ushkevich Valiantsin <vushkevih@gmail.com>
2025-04-25 20:19:07 +0200

    feate: Create Entities/Models. Added User, Booking, and Room entities for the hotel data model

---
+++ src/main/java/com/hotel/Hotel/entity/User.java
@@ -0,0 +1,68 @@
+package com.hotel.Hotel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.swing.text.SimpleAttributeSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email is required")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    private String password;
+    private String role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
