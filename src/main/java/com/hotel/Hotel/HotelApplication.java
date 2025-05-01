ace57ee92b49756ac4ee50557b415f32f3d3c3f5
Ushkevich Valiantsin <vushkevih@gmail.com>
2025-05-01 18:17:07 +0200

    fix: 	1) dependency for work with dev.env is added in pom.xml;         2) add automatic loading of AWS keys from .env file in main class;         3) all AWS keys are added in .env ;         4) file .env is added in .gitignore

--- src/main/java/com/hotel/Hotel/HotelApplication.java
+++ src/main/java/com/hotel/Hotel/HotelApplication.java
@@ -1,12 +1,18 @@
 package com.hotel.Hotel;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // загружает переменные из dev.env
+		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
 		SpringApplication.run(HotelApplication.class, args);
 	}
 
