72baf8c798cb009c971bd9cfdf8f53b58faeb4ed
Ushkevich Valiantsin <vushkevih@gmail.com>
2025-05-04 02:41:30 +0200

    feat: Added a Userservice interface with methods for user management and authentication

---
+++ src/main/java/com/hotel/Hotel/service/interfac/IUserService.java
@@ -0,0 +1,15 @@
+package com.hotel.Hotel.service.interfac;
+
+import com.hotel.Hotel.dto.LoginRequest;
+import com.hotel.Hotel.dto.Response;
+import com.hotel.Hotel.entity.User;
+
+public interface IUserService {
+    Response register(User user);
+    Response login(LoginRequest loginRequest);
+    Response getAllUsers();
+    Response getUserBookingHistory(String userId);
+    Response deleteUser(String userId);
+    Response getUserById(String userId);
+    Response getMyInfo(String userId);
+}
