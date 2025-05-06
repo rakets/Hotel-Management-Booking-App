028a2f887aad2d3bfeb67314ed7a20ee74c50db7
Ushkevich Valiantsin <vushkevih@gmail.com>
2025-05-06 16:41:53 +0200

    fix: updated Optional handling in BookingRepository
    Fixed the handling of Optional to ensure better handling of null values and avoid potential NullPointerExceptions.

--- src/main/java/com/hotel/Hotel/repo/BookingRepository.java
+++ src/main/java/com/hotel/Hotel/repo/BookingRepository.java
@@ -2,12 +2,15 @@ package com.hotel.Hotel.repo;
 
 import com.hotel.Hotel.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByRoomId(Long roomId);
-    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
     List<Booking> findByUserId(Long userId);
 
 
