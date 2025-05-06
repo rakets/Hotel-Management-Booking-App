3f5b6ab44c7282e7187157b32ba5eeefabf1b6b8
Ushkevich Valiantsin <vushkevih@gmail.com>
2025-05-06 19:37:21 +0200

    feat: implement user-related endpoints in UserController
    	- added /users/all endpoint to get all users (accessible only by admin)
    	- added /users/get-by-id/{userId} endpoint to get user by ID
    	- added /users/delete/{userId} endpoint to delete user by ID
    	- added /users/get-logged-in-profile-info endpoint to get current logged-in user profile info
    	- added /users/get-by-bookings/{userId} endpoint to get user booking history

---
+++ src/main/java/com/hotel/Hotel/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.hotel.Hotel.controller;
+
+import com.hotel.Hotel.dto.Response;
+import com.hotel.Hotel.service.interfac.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-by-id/{userId}")
+    public ResponseEntity<Response> getUserById(@PathVariable("userId") String userId){
+        Response response = userService.getUserById(userId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<Response> deleteUser(@PathVariable("userId") String userId){
+        Response response = userService.deleteUser(userId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-logged-in-profile-info")
+    public ResponseEntity<Response> getLoggedInUserProfile(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); //Authentication from security core
+        String email = authentication.getName();
+        Response response = userService.getMyInfo(email);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-by-bookings/{userId}")
+    public ResponseEntity<Response> getUserBookingHistory(@PathVariable("userId") String userId){
+        Response response = userService.getUserBookingHistory(userId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
