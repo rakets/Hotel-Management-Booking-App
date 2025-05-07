26c39b425bdfe51aebe154da8094208e16107b8b
Ushkevich Valiantsin <vushkevih@gmail.com>
2025-05-08 00:26:37 +0200

    fix: code to work with Dotenv is deleted

--- src/main/java/com/hotel/Hotel/HotelApplication.java
+++ src/main/java/com/hotel/Hotel/HotelApplication.java
@@ -1,19 +1,11 @@
 package com.hotel.Hotel;
-
-import io.github.cdimascio.dotenv.Dotenv;
+git
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // загружает переменные из dev.env
-		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-
 		SpringApplication.run(HotelApplication.class, args);
 	}
-
 }
